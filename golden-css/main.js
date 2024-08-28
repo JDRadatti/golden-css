9c489dbb1a7ce2ec9ab033de1f6439d123983781
Justin Radatti <jdradatti@gmail.com>
2024-08-28 15:59:45 -0700

    Add vite app to visualize style
    (idea in future to be able to adjust variables and output new
    stylesheets)

---
+++ golden-css/main.js
@@ -0,0 +1,46 @@
+import '../index.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <div class="display1 --xlg">Display 2</div>
+    <div class="display2">Display 1</div>
+    <div class="title1">Title 3</div>
+    <div class="title2">Title 2<div>
+    <div class="title3">Title 3</div>
+    <div class="heading">Heading</div>
+    <div class="subheading">Subheading</div>
+    <div class="body">Body text</div>
+    <div class="callout">Callout</div>
+    <div class="label">Label</div>
+    <div class="caption">Caption</div>
+    <div class="overline">Overline</div>
+  </div>
+  <div>
+    <p class="display1 --xlg">Display 2</p>
+    <p class="display2">Display 1</p>
+    <p class="title1">Title 3</p>
+    <p class="title2">Title 2<p>
+    <p class="title3">Title 3</p>
+    <p class="heading">Heading</p>
+    <p class="subheading">Subheading</p>
+    <p class="body">Body text</p>
+    <p class="callout">Callout</p>
+    <p class="label">Label</p>
+    <p class="caption">Caption</p>
+    <p class="overline">Overline</p>
+  </div>
+  <div>
+    <div class="display1__bold">Display 2</div>
+    <div class="display2__bold">Display 1</div>
+    <div class="title1__bold">Title 3</div>
+    <div class="title2__bold">Title 2<div>
+    <div class="title3__bold">Title 3</div>
+    <div class="heading__bold">Heading</div>
+    <div class="subheading__bold">Subheading</div>
+    <div class="body__bold">Body text</div>
+    <div class="callout__bold">Callout</div>
+    <div class="label__bold">Label</div>
+    <div class="caption__bold">Caption</div>
+    <div class="overline__bold">Overline</div>
+  </div>
+`
