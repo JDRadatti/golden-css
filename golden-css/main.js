33880e4f0d52e199d63e10b548571e8598d930b1
Justin Radatti <jdradatti@gmail.com>
2024-09-11 11:04:42 -0700

    Update main.js

--- golden-css/main.js
+++ golden-css/main.js
@@ -1,46 +1,95 @@
 import '../index.css'
+import '../js/button.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <div class="display1 --xlg">Display 2</div>
-    <div class="display2">Display 1</div>
-    <div class="title1">Title 3</div>
-    <div class="title2">Title 2<div>
+    <div class="display1"></div>
+    <div class="display2">Display 2</div>
+    <div class="title1">Title 1</div>
+    <div class="title2">Title 2</div>
     <div class="title3">Title 3</div>
     <div class="heading">Heading</div>
     <div class="subheading">Subheading</div>
-    <div class="body">Body text</div>
+    <div class="body">Body text here. THe quick brown fox jumped over the lazy dog. The quick brown fox jumpedd over the lazy dog. Ohh yeaddd.</div>
     <div class="callout">Callout</div>
     <div class="label">Label</div>
     <div class="caption">Caption</div>
     <div class="overline">Overline</div>
   </div>
   <div>
-    <p class="display1 --xlg">Display 2</p>
-    <p class="display2">Display 1</p>
-    <p class="title1">Title 3</p>
-    <p class="title2">Title 2<p>
-    <p class="title3">Title 3</p>
-    <p class="heading">Heading</p>
-    <p class="subheading">Subheading</p>
-    <p class="body">Body text</p>
-    <p class="callout">Callout</p>
-    <p class="label">Label</p>
-    <p class="caption">Caption</p>
-    <p class="overline">Overline</p>
-  </div>
-  <div>
-    <div class="display1__bold">Display 2</div>
-    <div class="display2__bold">Display 1</div>
-    <div class="title1__bold">Title 3</div>
-    <div class="title2__bold">Title 2<div>
-    <div class="title3__bold">Title 3</div>
-    <div class="heading__bold">Heading</div>
-    <div class="subheading__bold">Subheading</div>
-    <div class="body__bold">Body text</div>
-    <div class="callout__bold">Callout</div>
-    <div class="label__bold">Label</div>
-    <div class="caption__bold">Caption</div>
-    <div class="overline__bold">Overline</div>
+    <h1 class="display1 ">Display 1</h1>
+    <p class="display2 ">Display 2</p>
+    <p class="title1 ">Title 1</p>
+    <p class="title2 ">Title 2<p>
+    <p class="title3 ">Title 3</p>
+    <p class="heading ">Heading</p>
+    <p class="subheading ">Subheading</p>
+    <p class="body ">Body text</p>
+    <p class="callout ">Callout</p>
+    <p class="label ">Label</p>
+    <p class="caption ">Caption</p>
+    <p class="overline ">Overline</p>
   </div>
 `
+
+
+document.querySelector('#card').innerHTML = `
+<article> 
+    <header class="display1 m-bottom__s"> Header </header>
+    I am a card! 
+    <footer> footer </footer>
+</article>
+`
+
+document.querySelector('#button').innerHTML = `
+<div style="height: 10px"></div>
+<golden-button theme="bg-light__primary large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onprimary large outline" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onprimary large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+
+<div style="height: 10px"></div>
+<golden-button theme="bg-light__secondary large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onsecondary large outline" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onsecondary large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+
+<div style="height: 10px"></div>
+<golden-button theme="bg-light__tertiary large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__ontertiary large outline" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__ontertiary large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+
+<div style="height: 10px"></div>
+<golden-button theme="bg-light__error large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onerror large outline" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onerror large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+
+<div style="height: 10px"></div>
+<golden-button theme="bg-light__warning large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onwarning large outline" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onwarning large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+
+<div style="height: 10px"></div>
+<golden-button theme="bg-light__info large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__oninfo large outline" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__oninfo large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+
+<div style="height: 10px"></div>
+<golden-button theme="bg-light__success large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onsuccess large outline" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onsuccess large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+
+<div style="height: 10px"></div>
+<golden-button theme="bg-light__surface large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onsurface large outline" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__onsurface large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+
+<div style="height: 10px"></div>
+<golden-button theme="bg-light__inversesurface large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__inverseonsurface large outline" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+<golden-button theme="bg-light__inverseonsurface large" start-icon="arrow_forward" end-icon="home" label="Label"></golden-button>
+
+`
+/*  Article: data-type: horizontal, vertical
+ *  Style header, footer
+ *
+ *
+ * */
