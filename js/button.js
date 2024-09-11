c84094a7404ce3e9f966d765aade076560382409
Justin Radatti <jdradatti@gmail.com>
2024-09-11 11:06:14 -0700

    Add web component called 'golden-button'

---
+++ js/button.js
@@ -0,0 +1,64 @@
+class GoldenButton extends HTMLElement {
+    static observedAttributes = [
+        "href", "start-icon",
+        "end-icon", "label", "theme",
+    ];
+
+    constructor() {
+        super();
+
+        this.href = ""
+        this.theme = ""
+        this.startIcon = ""
+        this.endIcon = ""
+        this.label = ""
+    }
+
+    connectedCallback() {
+        var icons = ""
+        if (this.startIcon != "") {
+            icons += " start-icon"
+        }
+        if (this.endIcon != "") {
+            icons += " end-icon"
+        }
+        this.theme += icons
+        console.log(this.theme)
+
+        this.innerHTML = `
+<a href=\"${this.href}\" class=\"button w-inline-block ${this.theme}\"> 
+    <div class="state-layer"></div>
+    <div class="button__content">
+        ${this.startIcon}
+        <div class="body__bold">${this.label}</div>
+        ${this.endIcon}
+    </div>
+</a>
+`
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log("name, newval:", name, newValue)
+        switch (name) {
+            case "href":
+                this.href = newValue
+                break
+            case "start-icon":
+                console.log("start")
+                this.startIcon = `<div class=\"material-symbols-outlined\">${newValue}</div>`
+                break
+            case "end-icon":
+                this.endIcon = `<div class=\"material-symbols-outlined\">${newValue}</div>`
+                break
+            case "label":
+                this.label = newValue
+                break
+            case "theme":
+                this.theme = newValue
+                break
+        }
+    }
+}
+
+customElements.define("golden-button", GoldenButton);
+
